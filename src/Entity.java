537a21390a4750230008f78f9f8a0817ce83e4a6
schafkoepfer <38036591+schafkoepfer@users.noreply.github.com>
2018-04-10 14:08:44 +0200

    Revert "ToString"
    This reverts commit f9cc8c451d6b3f459e0d30a6d8f2cc052f9d2f4b.
    
    # Conflicts:
    #	src/Entity.java

--- src/Entity.java
+++ src/Entity.java
@@ -1,8 +1,6 @@
-import java.io.IOException;
-
 public abstract class Entity {
-    private final int ID;
-    private int energy;
+    protected final int ID;
+    protected int energy;
     protected final int startEnergy;
     protected XY position;
 
@@ -13,7 +11,7 @@ public abstract class Entity {
         this.position = position;
     }
 
-    public abstract void nextStep() throws IOException;
+    public abstract void nextStep();
 
     public XY getPosition() {
         return position;
@@ -23,37 +21,15 @@ public abstract class Entity {
         return energy;
     }
 
-    public void setEnergy(int newEnergy){
-        energy = newEnergy;
-        toString("Die neue Energie von " + this.getClass().getSimpleName() + " beträgt " + newEnergy + ".\n");
-        if(newEnergy < startEnergy)
-            energy = newEnergy;
-        else
-            energy = newEnergy;
-    }
-
     public int getID() {
         return ID;
-
     }
 
-    public int AddEnergy(int value){
-        if(energy + value <= startEnergy) {
-            energy += value;
-            return 0;
-        }
-        else{
-            int difference = energy + value - startEnergy;
-            energy = startEnergy;
-            return difference;
-        }
+    public void UpdateEnergy(int value){
+        energy += value;
     }
 
     public String toString(){
-        return this.getClass().getSimpleName() + ", Energy: " + energy + "/" + startEnergy + ", Pos: " + position.X + "/" + position.Y;
-    }
-
-    private static void toString(String string){
-        System.out.println(string);
+        return this.getClass().getSuperclass().getSimpleName() + ", Energy: " + energy + "/" + startEnergy + ", Pos: " + position.X + "/" + position.Y;
     }
 }
