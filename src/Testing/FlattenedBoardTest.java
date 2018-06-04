9fba64d982f1e9ac92ff495b3658c27c3a27094c
Martin-Develop <martin.neumann1@hs-augsburg.de>
2018-06-04 19:19:50 +0200

    FlattenedBoardTest

--- src/Testing/FlattenedBoardTest.java
+++ src/Testing/FlattenedBoardTest.java
@@ -1,67 +1,81 @@
 package Testing;
 
-import fatsquirrel.Logging;
 import fatsquirrel.XY;
 import fatsquirrel.core.Board;
-import fatsquirrel.core.Entities.*;
+import fatsquirrel.core.Entities.EntityContext;
 import fatsquirrel.core.Entities.PlayerEntities.MasterSquirrel;
-import fatsquirrel.core.Entities.PlayerEntities.MiniSquirrel;
+import fatsquirrel.core.Entities.PlayerEntities.MasterSquirrelBot;
 import fatsquirrel.core.FlattenedBoard;
 import org.jmock.Mockery;
-import org.jmock.Expectations;
 import org.junit.Test;
+
+import java.io.IOException;
 
 import static org.junit.Assert.*;
 
 public class FlattenedBoardTest {
 
     Mockery context = new Mockery();
-    final Board board = context.mock(Board.class);
+    //final BoardView boardView = context.mock(BoardView.class);
 
+    private final Board board = new Board();
     private final FlattenedBoard flattenedBoard = new FlattenedBoard(15,15, board);
+    private final MasterSquirrel masterSquirrel = new MasterSquirrelBot(0,1000,new XY(1,1));
+
+
+
+    public FlattenedBoardTest() throws Exception {
+    }
 
     @Test
     public void getSize() {
     }
 
     @Test
-    public void tryMove(MiniSquirrel miniSquirrel, XY moveDirection) {
+    public void tryMove() {
     }
 
     @Test
-    public void tryMove(GoodBeast goodBeast, XY moveDirection) {
+    public void tryMove1() {
     }
 
     @Test
-    public void tryMove(BadBeast badBeast, XY moveDirection) {
+    public void tryMove2() {
     }
 
     @Test
-    public void tryMove(MasterSquirrel masterSquirrel, XY moveDirection) {
+    public void tryMove3() {
     }
 
     @Test
-    public void nearestPlayerEntity(XY pos) {
+    public void nearestPlayerEntity() {
     }
 
     @Test
-    public void kill(Entity entity) {
+    public void kill() {
     }
 
     @Test
-    public void killAndReplace(Entity entity) {
+    public void killAndReplace() {
     }
 
     @Test
-    public void getEntityType(XY xy) {
+    public void getEntityType() {
     }
 
     @Test
-    public void getEntityType(int x, int y) {
+    public void getEntityType1() {
     }
 
     @Test
-    public void spawn_Mini(XY direction, int energy, MasterSquirrel masterSquirrel) {
+    public void spawn_Mini() {
+
+        XY direction = new XY(1,0);
+        int energy = 100;
+
+        assertEquals("int x = 2",2, masterSquirrel.getPosition().x +direction.x);
+        assertEquals("int y = 1", 1, masterSquirrel.getPosition().y +direction.y);
+        //board.setNewMiniSquirrel(x, y, energy, masterSquirrel);
     }
 
     @Test
@@ -69,6 +83,6 @@ public class FlattenedBoardTest {
     }
 
     @Test
-    public void getEntityAt(int x, int y) {
+    public void getEntityAt() {
     }
 }
